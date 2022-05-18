cb7a12beec7ebd66676161d7ea26a35b5bf8df7d
Felipe Arancibia <farancibia@moveapps.cl>
2022-05-18 15:08:28 -0400

    testing ssh keys

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -8,7 +8,7 @@ const Order = () => {
     <div className={styles.Order}>
       <p>
         <span>03.25.21</span>
-        <span>6 articles</span>
+        <span>5 articles</span>
       </p>
       <p>$560.00</p>
       <Image src={arrow} alt="arrow" />
